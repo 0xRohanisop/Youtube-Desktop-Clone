6b92ccfcc902b10d9f5cc5ce37b06591531dc32b
ROHAN IS OP <116496018+0xRohanisop@users.noreply.github.com>
2022-11-24 00:43:53 +0530

    search box

---
+++ yt.js
@@ -0,0 +1,12 @@
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
